67f8e94ab789b5da959b1b2d54496e68d4e70276
Tash Dutton <tashdutton@gmail.com>
2020-02-27 11:05:03 -0800

    fixed pokemon

--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,12 @@
-pokemon.forEach(p => {
-  console.log(p.name);
+$(document).ready(function(){
+  for(var i=0;i<pokemon.length;i++){
+    $('.pokemon').append("<h2>"+pokemon[i].name+"</h2>");
+    $('.pokemon').append("<h3>"+"ID: "+pokemon[i].id+"</h3>");
+    $('.pokemon').append("<h3>"+"height: "+pokemon[i].height+"</h3>");
+    $('.pokemon').append("<ul>"+"moves: ");
+    for (var j=0; j<pokemon[i].moves.length; j++){
+      $('.pokemon').append("<li>"+pokemon[i].moves[j]+"</li>");
+    }
+    $('.pokemon').append("</ul>");
+  }
 });
